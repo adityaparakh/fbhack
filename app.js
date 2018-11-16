68cc33d72b096cc9616f922f887024c4b89396b0
metemorris <metemorris@hotmail.com>
2018-11-16 02:25:50 -0800

    a

--- app.js
+++ app.js
@@ -916,7 +916,7 @@ function getRandomInt(min, max) {
 function getUserInfo(sender_psid){
     let request = require('request');
     var options ={
-        url:'https://graph.facebook.com/1964122107006784?fields=first_name,last_name,profile_pic&access_token=EAAIKXN8ZAjBsBANToUfJbTPviKjhaQhvCky9jyAOKZArf0V25ensSdZCleC2sIg1Qv2MCa6x9PDRzin1YQCr3X57nWrP494Lfea71sAqTP7b4gQ7SKmJZBeIZAWZAwz6ZBeQu3PrqLZAYn3CGwcqC4TeEMI2KsTgjaRMTuApITEYCAZDZD',
+        url:'https://graph.facebook.com/'+String(sender_psid)+'?fields=first_name,last_name,profile_pic&access_token='+String(PAGE_ACCESS_TOKEN),
     }
     return new Promise(function(resolve, reject){
       return request.get(options, function(err, resp, body) {
