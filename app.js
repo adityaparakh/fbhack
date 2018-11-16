b712057832dff52dbdf8eb6e40a15d09bd80554e
metemorris <metemorris@hotmail.com>
2018-11-16 03:39:22 -0800

    a

--- app.js
+++ app.js
@@ -844,9 +844,12 @@ function handleMessage(sender_psid, received_message) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
+    console.log('beg');
     var initializePromise = getInConvo(sender_psid);
     console.log(initializePromise);
+    console.log('beg2');
     initializePromise.then(function(result){
+      console.log('beg3');
       userDetails = result;
       // Use user details from here
       response = {'text':userDetails};
