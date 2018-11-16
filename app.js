4945dd1e790f68fa16313cf14765e1e0c91776c9
metemorris <metemorris@hotmail.com>
2018-11-16 00:33:59 -0800

    push

--- app.js
+++ app.js
@@ -900,10 +900,10 @@ function getRandomInt(min, max) {
 
 function getUserInfo(sender_psid, callback){
     let request = require('request');
-    return request('https://graph.facebook.com/1964122107006784?fields=first_name,last_name,profile_pic&access_token=EAAIKXN8ZAjBsBANToUfJbTPviKjhaQhvCky9jyAOKZArf0V25ensSdZCleC2sIg1Qv2MCa6x9PDRzin1YQCr3X57nWrP494Lfea71sAqTP7b4gQ7SKmJZBeIZAWZAwz6ZBeQu3PrqLZAYn3CGwcqC4TeEMI2KsTgjaRMTuApITEYCAZDZD', { json: true }, (err, res, body) => {
-        if (err) {
-            return console.log(err); }
-        return callback(sender_psid,JSON.stringify(body.first_name))
+    request('https://graph.facebook.com/1964122107006784?fields=first_name,last_name,profile_pic&access_token=EAAIKXN8ZAjBsBANToUfJbTPviKjhaQhvCky9jyAOKZArf0V25ensSdZCleC2sIg1Qv2MCa6x9PDRzin1YQCr3X57nWrP494Lfea71sAqTP7b4gQ7SKmJZBeIZAWZAwz6ZBeQu3PrqLZAYn3CGwcqC4TeEMI2KsTgjaRMTuApITEYCAZDZD', { json: true }, (err, res, body) => {
+      if (err) {
+          return console.log(err); }
+      callback(sender_psid,JSON.stringify(body.first_name))
     });
 }
 // Handles messaging_postbacks events
